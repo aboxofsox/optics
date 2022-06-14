a0f933a1fcd7dc3c92545dce121870859bc9cf63
aboxofsox <idkwthfml@gmail.com>
2022-06-13 23:05:48 -0500

    add readme & update cmd

--- cmd/root.go
+++ cmd/root.go
@@ -10,18 +10,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "optics",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Test API endpoints.",
+	Long:  `Use Optics to test application endpoints directly from your terminal.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +40,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Optics 
+
+## Usage
+```
+Use Optics to test application endpoints directly from your terminal.
+
+Usage:
+  optics [command]
+
+Available Commands:
+  completion  Generate the autocompletion script for the specified shell
+  help        Help about any command
+  init        Initialize optics.toml
+  run         Run optics
+
+Flags:
+  -h, --help     help for optics
+  -y, --skip     Use an empy config.
+  -t, --toggle   Help message for toggle
+
+Use "optics [command] --help" for more information about a command.
+```
