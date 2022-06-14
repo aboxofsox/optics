e1804573197fe00c6b879a84727d2e1216a79827
aboxofsox <idkwthfml@gmail.com>
2022-06-13 23:10:40 -0500

    fixed build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,18 @@
-package = "github.com/aboxofsox/optics"
-package_split = (${package//\//})
+package="github.com/aboxofsox/optics"
+package_split=(${package//\//})
 package_name=${packaeg_split[-1]}
 
 platforms=("windows/amd64", "darwin/amd64", "linux/amd64")
 
 for platform in "${platforms[@]}"
 do
-    platform_split = (${platform//\//})
+    platform_split=(${platform//\//})
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
 
     output_name=$package_name'-'$GOOS'-'$GOARCH
 
-    if [ $GOOS = "windows"]; then
+    if [ $GOOS="windows"]; then
         output_name+='.exe'
     fi
 
