a1d528f5e3746526d1bd13314c7a5bd4e91be363
aboxofsox <idkwthfml@gmail.com>
2022-06-17 00:17:26 -0500

    remove admin requirements

--- install/windows/install.ps1
+++ install/windows/install.ps1
@@ -1,23 +1,9 @@
 # Simple Windows install script
 
-# Set Execution-Policy
-Set-ExecutionPolicy Unrestricted
-
-# Check if ran as admin.
-# If not, exit the script.
-$IsAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
-
-If ($IsAdmin -ne $True) {
-    Write-Host 'The install script needs to be ran as administrator.' -ForegroundColor Red
-    Exit
-}
-
-# Set the install path.
-# If it doesn't exist, create it.
-$InstallPath = "$($Env:ProgramFiles)\\optics"
+$InstallPath = "C:\\Users\\$($Env:USERNAME)\\optics"
 
 If ((Test-Path -Path $InstallPath) -ne $True) {
-    New-Item -Path $Env:ProgramFiles -Name 'optics' -ItemType 'directory' | Out-Null
+    New-Item -Path $InstallPath -Name 'optics' -ItemType 'directory' | Out-Null
 }
 
 # Create a temporary file to hold the download.
